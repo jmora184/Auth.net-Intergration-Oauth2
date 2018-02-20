c99d6ed2d7bb2f67444e6afe32f6ca967caf72c9
Jeremiah Mora <jmora1845@gmail.com>
2018-02-20 09:45:03 -0800

    New Update

--- UnitTestW19/UnitTest1.cs
+++ UnitTestW19/UnitTest1.cs
@@ -20,5 +20,30 @@ namespace UnitTestW19
             // Assert
             Assert.AreEqual("Index1", result.ViewName);
         }
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
